bdf0b129be5c2250ab0e559a940604f01c6b56b8
Sarath G <Sarath G@ho.rnt.ai>
2022-09-26 17:38:09 +0530

    Bug fixed, functionality done

---
+++ EPAdmin/AdMaster/URLacssess.js
@@ -0,0 +1,35 @@
+ $(document).ready(function(){
+	
+	
+   $('#siteadmin').hide();
+	SP.SOD.executeOrDelayUntilScriptLoaded(AutoRedirect,'sp.js');
+   });	
+                
+               function AutoRedirect() {
+    var clientContext = new SP.ClientContext.get_current();
+    var currentUser = clientContext.get_web().get_currentUser();
+    clientContext.load(currentUser);
+
+    var userGroups = currentUser.get_groups();
+    clientContext.load(userGroups);
+    clientContext.executeQueryAsync(function(){
+         var groupsEnumerator = userGroups.getEnumerator();
+          while (groupsEnumerator.moveNext()) {
+             var group= groupsEnumerator.get_current();
+             console.log("group.get_title()",group);               
+             if(group.get_title() == "EventAdmin") {
+               window.location.href = _spPageContextInfo.webAbsoluteUrl + "/SitePages/Admin/home.aspx"
+               //alert("is admin");
+                $('#siteadmin').show();
+                 break;
+             }
+             else{
+              //alert("You do not have permission to access this page, contact your administrator to request access");
+            	window.location.href = "https://infornt.sharepoint.com/sites/RNTENG/SitePages/Employee/EmpMaster.aspx";
+             }
+   	   	}
+
+    }, function(err){
+        alert(err);
+    });
+}
---
+++ EPAdmin/AdMaster/active_class.js
@@ -0,0 +1,53 @@
+ 	
+ 	
+ 	var loginUserName = _spPageContextInfo.userDisplayName;
+	var RolePosition = _spPageContextInfo.isSiteAdmin;
+
+ 	$(document).ready(function() {
+ 	GetRoleList();
+    $(".mx-auto").click(function () {
+    $(".home").css("background", "transparent linear-gradient(89deg, #DE8601 0%, #FF3D97 100%) 0% 0% no-repeat padding-box");
+
+                $(this).addClass("active");     
+    });
+});
+
+
+
+function GetRoleList() {
+    $.ajax({
+        url: _spPageContextInfo.webAbsoluteUrl + "/_api/lists/getByTitle('EPAccessControl')/items?$select=*&$filter=(EmpName eq '"+loginUserName+"')",
+        method: "GET",
+        headers:
+           {
+               "Accept": "application/json;odata=verbose"
+           },
+           
+        success: function (data, status, xhr) {
+            dataresults = data.d.results;
+            console.log(dataresults);
+            for (var i = 0; i < dataresults.length; i++) {
+               Designation = data.d.results[i].Designation;
+               
+              // alert("I am a"+Designation);
+               if (Designation == "Admin"){
+               
+               
+               }else{
+                  $(".Access").css("pointer-events","none");
+
+                  $(".Access").css("opacity","0.3");
+                 // $(".emp-card span").css("display","none");
+                  //$(".emp-card input").css("display","none");
+                 //$(".search-box").css("display","none");
+
+               }
+               
+               //EventDates = moment(data.d.results[i].EventDate).format('YYYY-MM-DD');              
+               } // for loop End					                 
+        },
+        error: function (xhr, status, error) {
+            console.log("Failed");
+        }
+    });
+}
